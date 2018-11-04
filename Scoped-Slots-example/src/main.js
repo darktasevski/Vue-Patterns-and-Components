b81bdf53b96e7d71ed04db7b2b3799578f1d7184
Darkø Tasevski <darko.tasevski@redbox.com>
2018-11-04 14:27:25 +0100

    feat: Scoped Slots example

---
+++ Scoped-Slots-example/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+axios.defaults.headers.common['user-key'] = 'GET_YOUR_OWN_KEY'
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+Vue.config.productionTip = false
+
+new Vue({
+	render: h => h(App)
+}).$mount('#app')
